9e7da810f3fe3692bee3d9e4ba7551ca69861ac2
Niket Sharma <niketsharma22@gmail.com>
2023-03-29 20:27:48 +0530

    changes

--- src/practice/leetcode/PascalsTriangle.java
+++ src/practice/leetcode/PascalsTriangle.java
@@ -13,17 +13,11 @@ public class PascalsTriangle {
 	private static List<List<Integer>> generate(int numRows) {
 		var first = new ArrayList<Integer>();
 		first.add(1);
-		var second = new ArrayList<Integer>();
-		second.add(1);
-		second.add(1);
 		List<List<Integer>> result = new ArrayList<>();
 		if (numRows > 0) {
 			result.add(first);
 		}
-		if (numRows > 1) {
-			result.add(second);
-		}
-		for (int i = 3; i <= numRows; i++) {
+		for (int i = 2; i <= numRows; i++) {
 			List<Integer> innerList = new ArrayList<>();
 			innerList.add(1);
 			var prevList = result.get(i - 2);
